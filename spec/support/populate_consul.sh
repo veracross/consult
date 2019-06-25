02e743fbc3f48fb4a72cfec4b1caee9ee4c9fecb
aharpervc <aharper@breuer.com>
2019-06-25 17:39:14 -0400

    Normalize local resource addresses to `localhost` (#21) - this also fixes not being able to connect on Windows

--- spec/support/populate_consul.sh
+++ spec/support/populate_consul.sh
@@ -1,30 +1,30 @@
 curl \
     --request PUT \
     --data @spec/support/consul-services.json \
-    http://0.0.0.0:8500/v1/catalog/register
+    http://localhost:8500/v1/catalog/register
 
 curl \
     -H "X-Vault-Token: 94e1a9ed-5d72-5677-27ab-ebc485cca368" \
     -X POST \
     --data @spec/support/vault-test-data.json \
-    http://127.0.0.1:8200/v1/secret/data/database_credentials
+    http://localhost:8200/v1/secret/data/database_credentials
 
 curl \
     --request PUT \
     --data 'db1.local.net' \
-    http://0.0.0.0:8500/v1/kv/infrastructure/db1/dns
+    http://localhost:8500/v1/kv/infrastructure/db1/dns
 
 curl \
     --request PUT \
     --data $'Earth is the <%= vars[:earth] %>th element\n' \
-    http://0.0.0.0:8500/v1/kv/templates/elements/earth
+    http://localhost:8500/v1/kv/templates/elements/earth
 
 curl \
     --request PUT \
     --data $'Love is the <%= vars[:love] %>th element!\n' \
-    http://0.0.0.0:8500/v1/kv/templates/elements/love
+    http://localhost:8500/v1/kv/templates/elements/love
 
 curl \
     --request PUT \
     --data $'Aziz! <%= vars[:aziz] %>\n' \
-    http://0.0.0.0:8500/v1/kv/templates/elements/aziz
+    http://localhost:8500/v1/kv/templates/elements/aziz
