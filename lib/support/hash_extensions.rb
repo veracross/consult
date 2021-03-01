7268e944735a564ab2e59aedb2ebac47b1700a5e
Brian Borge <BrianBorge@users.noreply.github.com>
2021-03-01 16:07:18 -0500

    Use YAML.safe_load option to symbolize config keys (#30)

--- lib/support/hash_extensions.rb
+++ lib/support/hash_extensions.rb
@@ -3,21 +3,6 @@
 # ActiveSupport's corresponding methods
 #  - https://github.com/rails/rails/tree/master/activesupport/lib/active_support/core_ext/hash
 module HashExtension
-  def deep_symbolize_keys!(object = self)
-    case object
-    when Hash
-      object.keys.each do |key|
-        value = object.delete key
-        object[key.to_sym] = deep_symbolize_keys! value
-      end
-      object
-    when Array
-      object.map! { |e| deep_symbolize_keys! e }
-    else
-      object
-    end
-  end
-
   def deep_merge(other_hash, &block)
     merge(other_hash) do |key, this_val, other_val|
       if this_val.is_a?(Hash) && other_val.is_a?(Hash)
